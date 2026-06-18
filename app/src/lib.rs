0f988689c698f4fddc26da97cd211078968f25cc
ZacharyZcR <zacharyzcr1984@gmail.com>
2026-06-18 13:31:52 +0800

    Add i18n framework with zh-CN locale support
    Custom i18n crate (warp_i18n) with YAML locale files, t!() macro,
    and automatic locale detection via WARP_LANG / system locale.

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,5 +1,33 @@
 #![allow(clippy::doc_lazy_continuation)]
 
+#[macro_export]
+macro_rules! t {
+    ($key:literal, $($name:ident = $value:expr),+ $(,)?) => {
+        match $crate::i18n::t($key) {
+            value if value == $key => std::borrow::Cow::Owned(format!($key)),
+            value => $crate::i18n::interpolate(
+                value.as_ref(),
+                &[$((stringify!($name), format!("{}", $value))),+],
+            ),
+        }
+    };
+    ($key:literal, $($name:ident),+ $(,)?) => {
+        match $crate::i18n::t($key) {
+            value if value == $key => std::borrow::Cow::Owned(format!($key)),
+            value => $crate::i18n::interpolate(
+                value.as_ref(),
+                &[$((stringify!($name), format!("{}", $name))),+],
+            ),
+        }
+    };
+    ($key:literal) => {
+        match $crate::i18n::t($key) {
+            value if value == $key => std::borrow::Cow::Owned(format!($key)),
+            value => value,
+        }
+    };
+}
+
 mod ai;
 mod alloc;
 mod antivirus;
@@ -39,6 +67,7 @@ mod external_secrets;
 mod font_fallback;
 mod global_resource_handles;
 mod gpu_state;
+pub mod i18n;
 mod input_classifier;
 mod interval_timer;
 mod linear;
@@ -584,6 +613,8 @@ fn apply_scroll_multiplier(event: &mut Event, app: &AppContext) {
 /// Runs the app. If a subcommand was requested, it'll be run instead of the main application.
 #[::tracing::instrument(skip_all, fields(tags.cloud_agent = true))]
 pub fn run() -> Result<()> {
+    i18n::init_locale();
+
     // Perform any necessary platform-specific initialization.
     platform::init();
 
